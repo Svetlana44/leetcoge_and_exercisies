03ca1566a020058f4abe61da1920cebd416b844d
SpS <shchaaas@mail.ru>
2022-11-20 22:01:45 +0300

    TwoSum

---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSumFirstVariant(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int compl = target - nums[i];
+            if (map.containsKey(compl)) {
+                return new int[]{map.get(compl), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public int[] twoSumSecondVariant(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
