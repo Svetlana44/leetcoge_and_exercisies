d78662dbde066b69d6323c6ad4c74350f3e27294
SpS <shchaaas@mail.ru>
2022-11-25 22:22:45 +0300

    'PalindromeLinkedList'

---
+++ src/main/java/leetcode/PalindromeLinkedList.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class PalindromeLinkedList {
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public boolean isPalindrome(ListNode head) {
+return false;
+        }
+    }
+}
